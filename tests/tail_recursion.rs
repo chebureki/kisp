d038e45dee4e135e995a15d04e1895ac5875eace
Sokol, Kirill <kirill.sokol@uk-essen.de>
2023-03-28 12:39:12 +0200

    initial tests

---
+++ tests/tail_recursion.rs
@@ -0,0 +1,25 @@
+use kisp::assert_match;
+use kisp::testutils::quick_result;
+use kisp::value::{Copyable, EvalResult, EvalValue};
+use kisp::value::numeric::Numeric;
+
+#[test]
+fn sum_of_n(){
+    let (value, _) = quick_result(
+        "
+        (fn sum [n]
+            [
+                (fn iter [n acc]
+                    (if (>= 0 n)
+                        acc
+                        (iter (- n 1) (+ acc n))
+                    )
+                )
+                (iter n 0)
+            ]
+        )
+        (sum 100)
+        "
+    ).unwrap();
+    assert_match!(value, EvalValue::Copyable(Copyable::Numeric(Numeric::Integer(i))) if i==5050);
+}
